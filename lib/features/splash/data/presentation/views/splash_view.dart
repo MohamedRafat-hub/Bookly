8cb4fe114797beeaa631dbb3df126dff56ce5542
mohamedrafat <mtag66870@gmail.com>
2025-11-02 17:04:29 +0200

    first commit

---
+++ lib/features/splash/data/presentation/views/splash_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplashViewBody(),
+    );
+  }
+}
+
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
